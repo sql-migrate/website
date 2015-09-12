7e1cc103b8c24b142f54d2222ef7e331bf01ada0
felix <felix@volny.io>
2015-09-12 12:42:38 +0700

    Ajax contact form - broken validation
    Formspree's ajax not playing nicely with jqBootstrapvalidation.

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,25 @@
 ---
 ---
 
+// Floating label headings for the contact form
 $(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+});
 
+//jqBootstrapValidation
+$(function() {
   $("input,textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
-      // additional error messages or events
     },
-
+    submitSuccess: function($form, event) {
+    },
     filter: function() {
       return $(this).is(":visible");
     },
@@ -20,18 +31,26 @@ $(function() {
   });
 });
 
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-  $('#success').html('');
-});
-
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
+//Formspree
+var $contactForm = $('#contactForm');
+$contactForm.submit(function (e) {
+  e.preventDefault();
+  $.ajax({
+    url: '//formspree.io/{{ site.email }}',
+    method: 'POST',
+    data: $(this).serialize(),
+    dataType: 'json',
+    beforeSend: function() {
+      $('#send-button').hide();
+      $('#sending').show();
+    },
+    success: function (data) {
+      $('#sending').hide();
+      $('#sent-success').show();
+    },
+    error: function (err) {
+      $('#sending').hide();
+      $('#sent-error').show();
+    }
+  });
 });
